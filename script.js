1edcba4663389a29330e2c655f8a2f7a7067468d
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2025-11-14 13:26:51 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const divElement = document.querySelector(".container");
+
+const boxes = 800;
+for(let i=0;i<boxes;i++){
+	const dElement = document.createElement("div");
+	dElement.classList.add("square");
+
+	dElement.addEventListener("mouseover" , ()=>{
+		const randomColor = getRandomColor();
+		dElement.style.backgroundColor = randomColor;
+
+		setTimeout(()=>{
+			dElement.style.backgroundColor = #ddd;
+		},1000);		
+});
+		divElement.appendChild(dElement);
+}
+
+function getRandomColor(){
+	return `hsl(${Math.random() * 360}, 100%, 50%)`;
+}

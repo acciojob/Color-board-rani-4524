2b56ea44bf6a236e4ce3e59aa3010ccca5b3cc8d
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2025-12-03 13:17:55 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-//your JS code here. If required.
-const divElement = document.querySelector(".container");
+const container = document.querySelector('.container');
 
-const boxes = 800;
-for(let i=0;i<boxes;i++){
-	const dElement = document.createElement("div");
-	dElement.classList.add("square");
+const colors = [
+  "#ff4757", "#1e90ff", "#2ed573", "#ffa502",
+  "#3742fa", "#ff6b81", "#a29bfe", "#eccc68",
+  "#7bed9f", "#5352ed", "#ff7f50", "#2f3542"
+];
 
-	dElement.addEventListener("mouseenter" , ()=>{
-		const randomColor = getRandomColor();
-		dElement.style.backgroundColor = randomColor;
+for (let i = 0; i < 800; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
 
-		setTimeout(()=>{
-			dElement.style.backgroundColor = "#222";
-		},1000);		
-});
-		divElement.appendChild(dElement);
-}
+  square.addEventListener("mouseover", () => {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    square.style.background = randomColor;
 
-function getRandomColor(){
-	return `hsl(${Math.random() * 360}, 100%, 50%)`;
+    // remove color smoothly after 1 second
+    setTimeout(() => {
+      square.style.background = "#222";
+    }, 1000);
+  });
+
+  container.appendChild(square);
 }
